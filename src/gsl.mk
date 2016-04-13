6ec3519f73211240829d410305bf48145a044b08
Boudewijn Rempt <boud@valdyas.org>
2016-04-13 08:46:16 +0200

    Make shared, not static

--- src/gsl.mk
+++ src/gsl.mk
@@ -22,8 +22,7 @@ define $(PKG)_BUILD
         --host='$(TARGET)' \
         --build="`config.guess`" \
         --prefix='$(PREFIX)/$(TARGET)' \
-        --disable-shared \
-        --enable-static
+        --enable-shared 
     $(MAKE) -C '$(1)' -j '$(JOBS)'
     $(MAKE) -C '$(1)' -j 1 install
 
